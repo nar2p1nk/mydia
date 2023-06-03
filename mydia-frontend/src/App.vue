d783c0b20015b5eaeaff82d75aad479cccb1a06f
nar2p1nk <farrisisqandar@gmail.com>
2023-06-03 13:21:27 +0800

    made a vue app from vite with tailwindcss

---
+++ mydia-frontend/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+
+<template>
+  <h1 className="text-pink-800 ">Hellow</h1>
+</template>
+
---
+++ mydia-frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,vue}"
+  ],
+  theme: {
+    colors:{
+      "pink":'#ff189b',
+      "purple":"#352d68",
+      "lightBlack":"#1a1a1a",
+      "fontColor":"#ffffff"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
