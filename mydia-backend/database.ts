8b005191e80390214bdb51b44813b55eda70f247
nar2p1nk <farrisisqandar@gmail.com>
2023-06-25 23:49:15 +0800

    database schematic has been updated to have first and last name

--- mydia-backend/database.ts
+++ mydia-backend/database.ts
@@ -1,5 +1,6 @@
 import {PrismaClient} from "@prisma/client";
-const bcrypt = require('bcrypt');
+import { v4 } from "uuid";
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
@@ -10,6 +11,31 @@ async function getAllUsers(){
 
     return allUsers
 }
+
+async function createUser(
+    email:string,username:string,firstName:string,
+    lastName:string,password:any){
+
+    bcrypt.hash(password, 15,(err,hash)=>{
+        if(err){console.log(err);return err;}
+
+        prisma.user.create({
+            data:{
+                user_id:v4(),
+                email:email,
+                username:username,
+                firstName:firstName,
+                lastName:lastName,
+                password:hash,
+            }
+        })
+        console.log('user created');
+        
+
+    })
+}
+
+//createUser('yellow@pink.com','yellowPiss','Yellow','piss','eatass')
 
 getAllUsers()
     .then(async (v) => {
